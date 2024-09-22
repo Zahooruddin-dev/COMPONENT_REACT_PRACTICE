991bb55fb7c410169ff2b29750a5aca89c542eb3
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-22 05:21:17 +0500

    done

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -4,23 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Button from './components/button'
 import { FaAlignLeft, FaMoneyBill } from "react-icons/fa";
-/**
- * Challenge: Add an onClick handler to the Button
- * component instance (the one on this page). Then
- * make whatever changes you need for it to actually
- * work.
- * 
- * Clicking the button should log "Logging in..." 
- * to the console.
- */
-function logger(){
-  console.log('logging in');
-  
-}
+
 function App() {
   return (
     <main>
-      <Button onClick={logger} style={{color:'green'}}>  
+      <Button 
+      style={{color:'green'}}
+      size='sm'
+      >  
       <FaMoneyBill/>
          Buy now  
          </Button>
--- main/src/components/Button.jsx
+++ main/src/components/Button.jsx
@@ -1,5 +1,13 @@
-export default function Button({...rest}){
-  return (
-    <button {...rest}></button>
-  )
+export default function Button(props) {
+
+	let classNaam;
+
+	if (props.size === 'sm') {
+		console.log('sm');
+		classNaam = 'button-small';
+	} else if (props.size === 'lg') {
+		classNaam = 'button-large';
+	}
+
+	return <button className={classNaam}>{props.children}</button>;
 }
