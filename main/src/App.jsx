2d60a472aed304b22c25210f612cae311787a683
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-24 03:17:13 +0500

    1

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,31 +1,24 @@
-
 import './App.css';
 import Menu from './Menu/Menu';
 import MenuButton from './Menu/MenuButton';
-import MenuDropdown from './Menu/MenuDropdown';
-/**
- * Challenge:
- * 1. Convert the Menu component to use props.children
- *    instead of taking an `items` prop. (We'll update
- *    the MenuButton and MenuDropdown components later.)
- *    See note inside the Menu.js file for more info
- *
- * 2. import MenuButton and MenuDropdown into THIS file
- *    and render them as children of the Menu component.
- *    Remember to pass the buttonText and items array to
- *    the components that need those props to function.
- *    (We'll also be updating that soon!)
- *
- * NOTE: The functionality of the menu will be broken after
- * these changes, but that's okay! As such, don't worry
- * about moving the state or toggle function from the Menu;
- * there's more we need to learn before we can do that.
- */
+import MenuDropdown from './Menu/MenuDropdown';/**
+* Challenge:
+* 1. Convert the MenuButton to accept children and 
+*    render them. (In this case, the children will
+*    just be the button text)
+* 2. Change the MenuButton below to pass "Sports"
+*    in as a child of the component instead of by
+*    using the `buttonText` prop.
+* 
+* NOTE: It's fine that the menu is still broken, I
+* promise we're getting there! 😃
+*/
+
 function App() {
 	return (
 		<>
 			<Menu>
-				<MenuButton 					buttonText='Sports'></MenuButton>
+				<MenuButton>Sports</MenuButton>
 				<MenuDropdown
 					items={['Tennis', 'Pickleball', 'Racquetball', 'Squash']}
 				></MenuDropdown>
