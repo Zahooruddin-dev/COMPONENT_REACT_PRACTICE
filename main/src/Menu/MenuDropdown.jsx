dbfd5720b8c01a9e54b68731344aabbdd7617dde
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-24 05:51:45 +0500

    6

--- main/src/Menu/MenuDropdown.jsx
+++ main/src/Menu/MenuDropdown.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 export default function MenuDropdown({ children, open }) {
+  console.log(open);
+  
 	return open ?(
  <div className='menu-dropdown'>
       {children}
