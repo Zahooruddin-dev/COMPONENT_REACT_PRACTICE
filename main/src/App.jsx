1d7f8f9dd6780087b323a07c6a53e547fdc27c6f
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-25 06:32:06 +0500

    8

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -4,31 +4,18 @@ import Toggle from './Toggle/index';
 import { BsStar, BsStarFill } from "react-icons/bs"
 
 function App() {
-	/**
-	 * Challenge: refactor our app to use the Toggle.On
-	 * and Toggle.Off components to conditionally render the
-	 * star icons from react-icons. (See Star.js for that code).
-	 *
-	 * Reminders:
-	 * 1. Make sure to copy over the classNames from Star.js too!
-	 *    I've added some CSS that we don't want to lose.
-	 * 2. Clicking the Star should flip the Toggle state. Try to
-	 *    figure out how you can do that 🤔. Hint: we built something
-	 *    that lets us flip the Toggle state back and forth 😉
-	 */
-
 	return (
 		<>
 			<Toggle>
 				<Toggle.Button>
-				</Toggle.Button>
 				<Toggle.On>
 				<BsStarFill className="star filled" /> 
 				</Toggle.On>
 				<Toggle.Off>
-				<BsStarFill className="star filled" /> :
 				<BsStar className="star "/>
 				</Toggle.Off>
+				</Toggle.Button>
+	
 			</Toggle>
 		</>
 	);
