5d0c7e0ecd0854206ce1b3f6b3622a33d431fbd2
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-25 05:24:39 +0500

    1

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,38 +1,22 @@
 import React from 'react';
 import './App.css';
-import { BsStar, BsStarFill } from 'react-icons/bs';
-import Menu from './Menu/index';
-import Toggle from './Toggle/Toggle';
+import Star from './star';
+import Toggle from './Toogle';
 
 function App() {
-	const sports = ['Tennis', 'Pickleball', 'Racquetball', 'Squash'];
 
-	return (
-		<>
+
+
+  return (
+    <>
 			<Toggle>
-				<Toggle.Button>
-					<Toggle.On>
-						<BsStarFill className='star filled' />
-					</Toggle.On>
-					<Toggle.Off>
-						<BsStar className='star' />
-					</Toggle.Off>
-				</Toggle.Button>
+      	<Star />
 			</Toggle>
 
-			<br />
-			<hr />
-			<br />
-			<Menu>
-				<Menu.Button>Sports</Menu.Button>
-				<Menu.Dropdown>
-					{sports.map((sport) => (
-						<Menu.Item key={sport}>{sport}</Menu.Item>
-					))}
-				</Menu.Dropdown>
-			</Menu>
-		</>
-	);
+    </>
+
+		
+  )
 }
 
 export default App;
---
+++ main/src/Toogle.jsx
@@ -0,0 +1,19 @@
+/**
+ * Challenge:
+ * 
+ * 1. Create a Toggle component that initializes its own boolean state.
+ * 2. Create a function called `toggle` in that component that flips the 
+ *    boolean when the function runs.
+ * NOTE: Don't worry about rendering anything yet
+ */
+
+import React from "react"
+
+export default function Toggle() {
+
+  const[booleanStates, setBooleanStates] = React.useState(true)
+  function toggling()
+{
+  setBooleanStates(prevState=>!prevState)
+}
+}
