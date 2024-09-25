380caf917dda89e1d4347ca65e15c6236d8e4e2a
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-25 05:02:40 +0500

    headless components

--- main/src/Menu/Menu.jsx
+++ main/src/Menu/Menu.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
- 
-export const MenuContext = React.createContext();
+import Toggle from "../Toggle/index"
 
 export default function Menu({ children }) {
-	const [open, setOpen] = React.useState(true);
-
-	function toggle() {
-		console.log('toggle');
-
-		setOpen((prevOpen) => !prevOpen);
-	}
-	return (
-		<MenuContext.Provider value={true}>
-			<div className='menu'>{children}</div>
-		</MenuContext.Provider>
-	);
+    return (
+        <Toggle>
+            <div className="menu">
+                {children}
+            </div>
+        </Toggle>
+    )
 }
--- main/src/Menu/MenuItem.jsx
+++ main/src/Menu/MenuItem.jsx
@@ -1,10 +1,9 @@
 import React from "react"
 
 export default function MenuItem({ children }) {
-  
-  return (
-      <div className="menu-item">
-          {children}
-      </div>
-  )
+    return (
+        <div className="menu-item">
+            {children}
+        </div>
+    )
 }
