511cda57c8a7be7cd236125710a8c7a30bec798c
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-25 05:28:43 +0500

    2

--- main/src/Toogle.jsx
+++ main/src/Toogle.jsx
@@ -9,11 +9,12 @@
 
 import React from "react"
 
-export default function Toggle() {
+export default function Toggle({children}) {
 
   const[booleanStates, setBooleanStates] = React.useState(true)
   function toggling()
 {
   setBooleanStates(prevState=>!prevState)
 }
+return booleanStates && children
 }
