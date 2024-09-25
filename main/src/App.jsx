125b1d04b51d1f2053b8c6195df76b0630b544cc
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-25 09:46:01 +0500

    new component star

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,41 +1,28 @@
 import React from 'react';
 import './App.css';
 import Toggle from './components/Toggle/index';
-import { BsStar, BsStarFill } from "react-icons/bs"
 import Menu from './components/Menu/index';
-import MenuButton from './components/Menu/MenuButton';
-import MenuDropdown from './components/Menu/MenuDropdown';
+import Star from './components/star';
 function App() {
-
 	return (
 		<>
+		<Star></Star>
+			<br />
 			<Toggle>
-				<Toggle.Button>
-				<Toggle.On>
-				<BsStarFill className="star filled" /> 
-				</Toggle.On>
-				<Toggle.Off>
-				<BsStar className="star "/>
-				</Toggle.Off>
-				</Toggle.Button>
+				<Menu>
+					<Toggle.Button>
+						<Menu.Button>Menu</Menu.Button>
+					</Toggle.Button>
+					<Toggle.On>
+						<Menu.Dropdown>
+							<Menu.Item>Home</Menu.Item>
+							<Menu.Item>About</Menu.Item>
+							<Menu.Item>Resume</Menu.Item>
+							<Menu.Item>Project</Menu.Item>
+						</Menu.Dropdown>
+					</Toggle.On>
+				</Menu>
 			</Toggle>
-			<Toggle>
-			<Menu>
-			<Toggle.Button>
-			<Menu.Button>Menu</Menu.Button>
-			</Toggle.Button>
-			<Toggle.On>
-				<Menu.Dropdown>
-					<Menu.Item>Home</Menu.Item>
-					<Menu.Item>About</Menu.Item>
-					<Menu.Item>Resume</Menu.Item>
-					<Menu.Item>Project</Menu.Item>
-				</Menu.Dropdown>
-				</Toggle.On>
-
-			</Menu>
-			</Toggle>
-
 		</>
 	);
 }
