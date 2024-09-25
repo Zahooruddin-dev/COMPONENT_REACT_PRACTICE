e6974b6286747a286a2432a616a458d2c3af4007
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-25 05:33:23 +0500

    export toggle

--- main/src/Toggle/Toogle.jsx
+++ main/src/Toggle/Toogle.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { createContext } from 'react';
+export const outside = createContext();
 
 export default function Toggle({ children }) {
+	/**
+	 * Challenge: set up context!
+	 * Reminder of the steps:
+	 * 1. Create a new context (outside the component, but in this file)
+	 * 2. Export that context instance from the file so we
+	 *    can use it eleswhere
+	 * 3. Use the Context Provider to wrap the `children` returned
+	 *    from this Toggle component
+	 * 4. Pass the state values to the context value prop for access
+	 *    from child components later on
+	 */
+
 	const [booleanStates, setBooleanStates] = React.useState(true);
 	function toggling() {
 		setBooleanStates((prevState) => !prevState);
