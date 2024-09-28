1b6ed6fabc34203695f88ab1d30a85ad48779e61
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-28 10:28:47 +0500

    Update MenuDropdown.jsx

--- main/src/components/Menu/MenuDropdown.jsx
+++ main/src/components/Menu/MenuDropdown.jsx
@@ -3,7 +3,6 @@ import { useContext } from "react"
 import { MenuContext } from "./Menu"
 export default function MenuDropdown({ children }) {
     const {open} = useContext(MenuContext)
-    const {toggleOpen} = useContext(MenuContext)
 
     return  (
         open &&
