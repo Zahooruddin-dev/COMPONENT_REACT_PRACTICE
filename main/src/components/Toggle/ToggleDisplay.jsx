5d74821750217f57089bf743b8daaab4e1df60c6
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-28 09:02:57 +0500

    2

--- main/src/components/Toggle/ToggleDisplay.jsx
+++ main/src/components/Toggle/ToggleDisplay.jsx
@@ -1,5 +1,22 @@
-import React from "react"
+import React, { Children } from "react"
+import { ToggleContext } from "./Toggle"
 
-export default function ToggleDisplay(){
+export default function ToggleDisplay({Children}){
+const {on} = React.useContext(ToggleContext)
+  return(
+    <>
+    {Children(on)}
+    </>
+  )
 
 }
+/**
+ * Challenge: 
+ * Part 1: 
+ * Create a ToggleDisplay component and attach
+ * it to Toggle as Toggle.Display.
+ * 
+ * It should grab the `on` value from the Toggle context
+ * and render children by calling children as a function, 
+ * passing the `on` value to it. (E.g. children(on)).
+ */
