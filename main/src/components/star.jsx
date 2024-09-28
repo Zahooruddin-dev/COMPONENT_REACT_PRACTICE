e8c3ed09b00cf58fe0536e1d4a55b5e33de9e5af
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-28 10:04:32 +0500

    2

--- main/src/components/star.jsx
+++ main/src/components/star.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import Toggle from './Toggle/index';
 import { BsStar, BsStarFill } from 'react-icons/bs';
+import useToggle from './hooks/useToggle';
+export default function Star({ onChange }) {
 
-export default function Star({onChange}) {
-
+	const [on, toggle] = useToggle();
 	return (
-		<Toggle onToggle={onChange}>
-			<Toggle.Button>
-				<Toggle.On>
-					<BsStarFill className='star filled' />
-				</Toggle.On>
-				<Toggle.Off>
-					<BsStar className='star ' />
-				</Toggle.Off>
-			</Toggle.Button>
-		</Toggle>
+		<>
+			{on ? (
+				<BsStarFill onClick = {toggle} className='star filled' />
+			) : (
+				<BsStar onClick = {toggle} className='star ' />
+			)}
+		</>
 	);
 }
