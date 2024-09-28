835a22844ec5b40eab501fbe851648a2b563e948
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-28 10:32:47 +0500

    8

--- main/src/components/Menu/Menu.jsx
+++ main/src/components/Menu/Menu.jsx
@@ -3,15 +3,6 @@ import useToggle from '../hooks/useToggle';
 const MenuContext= createContext()
 export {MenuContext}
 export default function Menu({ children, onOpen }) {
-/**
- * Challenge:
- * 6. Access the needed context values in the MenuButton
- *    and MenuDropdown components. See if you can remember
- *    (or figure out again) how those values are used in each 
- *    of those components. Once you've done this successfully,
- *    the Menu should start working again (except for the onOpen
- *    callback, which we'll work on fixing soon).
- */
 
 	const [open, toggleOpen] = useToggle();
 	
